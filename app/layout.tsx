e5c3f5a6f3ddc3754aeb410d38bd1ad5c577cac6
Yado3230 <yaredbest81@gmail.com>
2023-08-12 11:56:05 +0300

    create store modal is created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
+import type { Metadata } from "next";
+import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,16 +1,66 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
-  <Modal
-    title="Create Store"
-    description="Add a new store to manage products and categories"
-    isOpen={storeModal.isOpen}
-    onClose={storeModal.onClose}
-  >
-    Future Create Store Form
-  </Modal>;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO: CREATE STORE
+  };
+
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form action="" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name:</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-Commerse" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
 };
