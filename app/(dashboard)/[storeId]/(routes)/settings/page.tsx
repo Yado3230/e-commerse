1f07fba8a5a17859496176ef2b9eae6816e3caeb
Yado3230 <yaredbest81@gmail.com>
2023-08-15 15:05:49 +0300

    next cloudinary is used to manage image upload

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,7 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { SettingsForm } from "./components/setttings-form";
+import { SettingsForm } from "./components/settings-form";
 
 interface SettingsPageProps {
   params: {
---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) null;
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                variant="destructive"
+                size="sm"
+                onClick={() => onRemove(url)}
+              >
+                <Trash className="w-4 h-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="ogrczh2z">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              type="button"
+              variant="secondary"
+              disabled={disabled}
+              onClick={onClick}
+            >
+              <ImagePlus className="w-4 h-4 mr-2" />
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
+export default ImageUpload;
